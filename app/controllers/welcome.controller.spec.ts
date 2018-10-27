50058bcb77c4ce3d08253d58baf8ae28f464836c
maxvektor <m.kocijevsky@gmial.com>
2018-10-27 19:17:16 +0300

    fix eslint issue, lint all code

--- app/controllers/welcome.controller.spec.ts
+++ app/controllers/welcome.controller.spec.ts
@@ -1,22 +1,14 @@
 import * as request from "supertest";
 import app from "../server";
 
-describe('GET /welcome', () => {
-  it('should return 200 OK', () => {
-    return request(app)
-      .get('/welcome')
+describe("GET /welcome", () => {
+  test("should return 200 OK", () => request(app)
+      .get("/welcome")
       .expect(200)
-      .then(res => {
-        expect(res.text).toBe('Hello, World!');
-      });
-  });
-  
-  it('should return 200 OK', () => {
-    return request(app)
-      .get('/welcome/drug')
+      .then(res => expect(res.text).toBe("Hello, World!")));
+
+  test("should return 200 OK", () => request(app)
+      .get("/welcome/drug")
       .expect(200)
-      .then(res => {
-        expect(res.text).toBe('Hello, drug');
-      });
-  });
+      .then(res => expect(res.text).toBe("Hello, drug")));
 });
