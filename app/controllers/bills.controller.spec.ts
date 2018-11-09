2dfe1fae9e63e3871457aa8668fb15e8179c8d5d
maxvektor <m.kocijevsky@gmial.com>
2018-11-09 20:44:02 +0200

    first test for endpoint using firestore

--- app/controllers/bills.controller.spec.ts
+++ app/controllers/bills.controller.spec.ts
@@ -1,7 +1,20 @@
+/* eslint import/order: 0 import/first:0 */
+
+/**
+ * jest.mock should apear in the top of the file
+ * @see https://github.com/kulshekhar/ts-jest/wiki/Troubleshooting
+ */
+const fireMock = require("../../mocks/firestoreMock");
+const MockFirebase = require("mock-cloud-firestore");
+
+jest.mock("../firebase", () => ({
+  default: new MockFirebase(fireMock).firestore()
+}));
+
 import * as request from "supertest";
 import app, { server } from "../server";
 
-xdescribe("GET /bills", () => {
+describe("GET /bills", () => {
   afterAll(done => {
     server.close(done);
   });
@@ -11,7 +24,11 @@ xdescribe("GET /bills", () => {
       .get("/bills")
       .expect(200)
       .then(res => {
-        expect(res.text).toBe("Hello, World!");
+        expect(JSON.parse(res.text)).toEqual([
+          { id: "bills_doc_1", data: { hello: "bills_doc_1" } },
+          { id: "bills_doc_2", data: { hello: "bills_doc_2" } },
+          { id: "bills_doc_3", data: { hello: "bills_doc_3" } }
+        ]);
         done();
       }));
 });
